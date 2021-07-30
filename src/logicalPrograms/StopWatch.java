3a3db003ff1117913294e5944e0c3a66082d3e14
vismayatk002 <vismayatk002@gmail.com>
2021-07-30 18:01:17 +0530

    Completed stopwatch program

--- src/logicalPrograms/StopWatch.java
+++ src/logicalPrograms/StopWatch.java
@@ -1,5 +1,22 @@
 package logicalPrograms;
 
-public class StopWatch {
+import java.util.Scanner;
 
+public class StopWatch {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Press 1 to start Stopwatch :");
+		int s = sc.nextInt();
+		if(s == 1) {
+			long start = System.currentTimeMillis();
+			System.out.print("\nPress any number to stop Stopwatch :");
+			int c = sc.nextInt();
+			long stop = System.currentTimeMillis();
+			float timeElapsed = (stop - start) / 1000;
+			System.out.printf("Elapsed Time : %.2f " , timeElapsed);
+		}
+		sc.close();
+	}
 }
